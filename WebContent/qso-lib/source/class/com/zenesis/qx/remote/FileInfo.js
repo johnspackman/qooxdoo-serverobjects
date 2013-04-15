a6176e67f081f41de3374ba3a2d25065ea1bfaf5
John Spackman <john.spackman@zenesis.com>
2013-04-15 17:15:56 +0100

    licensing comment

--- WebContent/qso-lib/source/class/com/zenesis/qx/remote/FileInfo.js
+++ WebContent/qso-lib/source/class/com/zenesis/qx/remote/FileInfo.js
@@ -1,3 +1,25 @@
+/* ************************************************************************
+
+   server-objects - a contrib to the Qooxdoo project (http://qooxdoo.org/)
+
+   http://qooxdoo.org
+
+   Copyright:
+     2010 Zenesis Limited, http://www.zenesis.com
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     
+     This software is provided under the same licensing terms as Qooxdoo,
+     please see the LICENSE file in the Qooxdoo project's top-level directory 
+     for details.
+
+   Authors:
+     * John Spackman (john.spackman@zenesis.com)
+
+************************************************************************ */
+
 /**
  * Immutable representation of a file returned from the server (FileApi.FileInfo)
  */
