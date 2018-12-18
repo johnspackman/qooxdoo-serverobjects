874260497380d846a690f4c4b7ee4a01e5190aac
John Spackman <john.spackman@zenesis.com>
2018-12-18 09:42:31 +0000

    refactor object mapper

--- src/java/com/zenesis/qx/remote/ProxyObjectMapper.java
+++ src/java/com/zenesis/qx/remote/ProxyObjectMapper.java
@@ -57,7 +57,7 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
  * @author <a href="mailto:john.spackman@zenesis.com">John Spackman</a>
  */
 @SuppressWarnings("serial")
-public class ProxyObjectMapper extends ObjectMapper {
+public class ProxyObjectMapper extends BasicObjectMapper {
 	
 	@SuppressWarnings("unused")
 	private static final Logger log = org.apache.logging.log4j.LogManager.getLogger(ProxyObjectMapper.class);
@@ -81,196 +81,6 @@ public class ProxyObjectMapper extends ObjectMapper {
 		}
 	}
 	
-	/*
-	 * Serialises a string, encoding unicode characters
-	 */
-	private static final class StringSerializer extends JsonSerializer<String> {
-
-		final char[] HEX_CHARS = "0123456789ABCDEF".toCharArray();
-		final int[] ESCAPE_CODES = CharTypes.get7BitOutputEscapes();
-
-		/* (non-Javadoc)
-		 * @see com.fasterxml.jackson.databind.JsonSerializer#serialize(java.lang.Object, com.fasterxml.jackson.core.JsonGenerator, com.fasterxml.jackson.databind.SerializerProvider)
-		 */
-		@Override
-		public void serialize(String value, JsonGenerator gen, SerializerProvider sp) throws IOException, JsonProcessingException {
-			int status = ((JsonWriteContext) gen.getOutputContext()).writeValue();
-			switch (status) {
-			case JsonWriteContext.STATUS_OK_AFTER_COLON:
-				gen.writeRaw(':');
-				break;
-				
-			case JsonWriteContext.STATUS_OK_AFTER_COMMA:
-				gen.writeRaw(',');
-				break;
-				
-			case JsonWriteContext.STATUS_EXPECT_NAME:
-				throw new JsonGenerationException("Can not write string value here");
-			}
-			gen.writeRaw('"');
-			for (char c : value.toCharArray()) {
-				if (c >= 0x80)
-					writeUnicodeEscape(gen, c); // use generic escaping for all
-												// non US-ASCII characters
-				else {
-					// use escape table for first 128 characters
-					int code = (c < ESCAPE_CODES.length ? ESCAPE_CODES[c] : 0);
-					if (code == 0)
-						gen.writeRaw(c); // no escaping
-					else if (code < 0)
-						writeUnicodeEscape(gen, (char) (-code - 1)); // generic escaping
-					else
-						writeShortEscape(gen, (char) code); // short escaping (\n \t ...)
-				}
-			}
-			gen.writeRaw('"');
-		}
-		
-		private void writeUnicodeEscape(JsonGenerator gen, char c)
-				throws IOException {
-			gen.writeRaw('\\');
-			gen.writeRaw('u');
-			gen.writeRaw(HEX_CHARS[(c >> 12) & 0xF]);
-			gen.writeRaw(HEX_CHARS[(c >> 8) & 0xF]);
-			gen.writeRaw(HEX_CHARS[(c >> 4) & 0xF]);
-			gen.writeRaw(HEX_CHARS[c & 0xF]);
-		}
-
-		private void writeShortEscape(JsonGenerator gen, char c)
-				throws IOException {
-			gen.writeRaw('\\');
-			gen.writeRaw(c);
-		}
-	}
-
-	/*
-	 * Serialises enums in camelCase
-	 */
-	private static final class EnumSerializer extends JsonSerializer<Enum> {
-		/* (non-Javadoc)
-		 * @see org.codehaus.jackson.map.JsonSerializer#serialize(java.lang.Object, org.codehaus.jackson.JsonGenerator, org.codehaus.jackson.map.SerializerProvider)
-		 */
-		@Override
-		public void serialize(Enum value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
-			if (value == null)
-				jgen.writeNull();
-			else
-				jgen.writeString(Helpers.serialiseEnum(value));
-		}
-
-	};
-	
-/*	
-	private static final class EnumDeserializer extends JsonDeserializer<Enum> {
-
-		@Override
-		public Enum deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-			String value = jp.getText();
-			if (value == null || value.length() == 0)
-				return null;
-			return null;
-		}
-		
-	}
-*/
-	
-	/*
-	 * Serialises Maps
-	 */
-	private static final class MapSerializer extends JsonSerializer<Map> {
-		
-		/* (non-Javadoc)
-		 * @see org.codehaus.jackson.map.JsonSerializer#serialize(java.lang.Object, org.codehaus.jackson.JsonGenerator, org.codehaus.jackson.map.SerializerProvider)
-		 */
-		@Override
-		public void serialize(Map map, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
-			if (map == null)
-				jgen.writeNull();
-			else {
-				jgen.writeStartObject();
-				for (Object key : map.keySet()) {
-					if (key == null)
-						continue;
-					if (key instanceof Enum)
-						jgen.writeFieldName(Helpers.serialiseEnum((Enum)key));
-					else
-						jgen.writeFieldName(key.toString());
-					Object value = map.get(key);
-					if (value == null)
-						jgen.writeNull();
-					else
-						jgen.writeObject(value);
-				}
-				jgen.writeEndObject();
-			}
-		}
-
-	};
-	
-	/*
-	 * Serialises a file, but only showing the part of the path relative to the "root" dir of
-	 * the web application
-	 */
-	public static final class FileSerializer extends JsonSerializer<File> {
-		
-		private final String localPrefix;
-		
-		public FileSerializer(File localDir) {
-			this.localPrefix = localDir == null ? null : localDir.getAbsolutePath() + File.separatorChar;
-		}
-
-		/* (non-Javadoc)
-		 * @see com.fasterxml.jackson.databind.JsonSerializer#serialize(java.lang.Object, com.fasterxml.jackson.core.JsonGenerator, com.fasterxml.jackson.databind.SerializerProvider)
-		 */
-		@Override
-		public void serialize(File value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
-			if (value == null)
-				jgen.writeNull();
-			else {
-				String str = value.getAbsolutePath();
-				if (localPrefix == null)
-					jgen.writeString(str);
-				else {
-					int len = localPrefix.length();
-					if (len < str.length() && str.substring(0, len).equalsIgnoreCase(localPrefix))
-						jgen.writeString(str.substring(len));
-					else
-						jgen.writeString(str);
-				}
-			}
-		}
-	}
-	
-	/*
-	 * Deserialises a file, keeping it relative to the root of the web application
-	 */
-	public static final class FileDeserializer extends JsonDeserializer<File> {
-		
-		private final File localDir;
-		
-		public FileDeserializer(File localDir) {
-			this.localDir = localDir;
-		}
-
-		/* (non-Javadoc)
-		 * @see com.fasterxml.jackson.databind.JsonDeserializer#deserialize(com.fasterxml.jackson.core.JsonParser, com.fasterxml.jackson.databind.DeserializationContext)
-		 */
-		@Override
-		public File deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-			String value = jp.getText();
-			if (value == null || value.length() == 0)
-				return null;
-			if (localDir == null)
-				return new File(value);
-			if (File.separatorChar == '/' && value.charAt(0) == '/')
-				return new File(value);
-			if (File.separatorChar == '\\' && value.length() > 1 && value.charAt(1) == ':')
-				return new File(value);
-			File file = new File(localDir, value);
-			return file;
-		}
-
-	}
 	
 	/** the tracker */
 	private final ProxySessionTracker tracker;
@@ -300,27 +110,13 @@ public class ProxyObjectMapper extends ObjectMapper {
 	 * @param rootDir root directory to serialise all File's as relative to
 	 */
 	public ProxyObjectMapper(ProxySessionTracker tracker, boolean indent, File rootDir) {
-		super();
-		if (rootDir == null)
-			rootDir = new File(".");
-		if (indent)
-			this.enable(SerializationFeature.INDENT_OUTPUT);
+		super(indent, rootDir == null ? new File(".") : null);
 		this.tracker = tracker;
-		SimpleModule module;
-		
-		module = new SimpleModule("ProxyObjectMapper1", Version.unknownVersion());
-		module.addSerializer(Enum.class, new EnumSerializer());
-		registerModule(module);
-		
-		module = new SimpleModule("ProxyObjectMapper2", Version.unknownVersion());
-		module.addSerializer(Date.class, new DateSerializer());
-		module.addSerializer(String.class, new StringSerializer());
-		module.addSerializer(File.class, new FileSerializer(rootDir));
-		module.addDeserializer(File.class, new FileDeserializer(rootDir));
-		module.addSerializer(Map.class, new MapSerializer());
-		module.addSerializer(Proxied.class, new ProxiedSerializer());
-		module.addDeserializer(Proxied.class, new ProxiedDeserializer());
-		registerModule(module);
+	}
+	
+	@Override
+	protected void addToModule(SimpleModule module) {
+        module.addSerializer(Date.class, new DateSerializer());
 	}
 	
 	/**
@@ -329,24 +125,4 @@ public class ProxyObjectMapper extends ObjectMapper {
 	public ProxySessionTracker getTracker() {
 		return tracker;
 	}
-
-	/**
-	 * Enables or disabled quoted field names
-	 * @param set
-	 */
-	public void setQuoteFieldNames(boolean set) {
-		if (set)
-			getJsonFactory().enable(JsonGenerator.Feature.QUOTE_FIELD_NAMES);
-		else
-			getJsonFactory().disable(JsonGenerator.Feature.QUOTE_FIELD_NAMES);
-	}
-	
-	/**
-	 * Whether field names will be quoted
-	 * @return
-	 */
-	public boolean isQuoteFieldNames() {
-		return getJsonFactory().isEnabled(JsonGenerator.Feature.QUOTE_FIELD_NAMES);
-	}
-
 }
