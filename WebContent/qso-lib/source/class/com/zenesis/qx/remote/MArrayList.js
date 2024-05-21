7c0d3d41ccc41e7e370e37d903e270b70116e016
Patryk Milinowski <pmilinowski@vmn.digital>
2024-05-21 10:39:51 +0100

    MArrayList: implement checking for nulls on the client

--- WebContent/qso-lib/source/class/com/zenesis/qx/remote/MArrayList.js
+++ WebContent/qso-lib/source/class/com/zenesis/qx/remote/MArrayList.js
@@ -5,6 +5,18 @@ qx.Mixin.define("com.zenesis.qx.remote.MArrayList", {
 
   members: {
     __onArrayListChange(evt) {
+      this.__detectNulls();
+      this.__detectDuplicates();
+    },
+
+    /**
+     * Checks for duplicates.
+     * If a duplicate is found and property detectDuplicates is set to true, `debugger;` is called..
+     * @returns
+     */
+    __detectDuplicates() {
+      if (!this.getDetectDuplicates()) return true;
+
       let arr = this.toArray();
       for (let i = 0; i < arr.length; i++) {
         let obj = arr[i];
@@ -13,6 +25,23 @@ qx.Mixin.define("com.zenesis.qx.remote.MArrayList", {
             console.error("Detected duplicate in arraylist");
             debugger;
           }
+        }
+      }
+    },
+
+    /**
+     * Checks for null values.
+     * If a null is found and property detectNulls is set to true, `debugger;` is called..
+     * @returns
+     */
+    __detectNulls() {
+      if (!this.getDetectNulls()) return true;
+
+      let arr = this.toArray();
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === null) {
+          console.error("Detected null in arraylist");
+          debugger;
         }
       }
     }
