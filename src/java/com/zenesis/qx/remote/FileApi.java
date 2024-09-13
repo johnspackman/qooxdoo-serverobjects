f6b3f5114708c1c84a01cc0bd2c84f0a8052745c
Patryk Milinowski <pmilinowski@vmn.digital>
2024-09-13 11:32:19 +0100

    formatting

--- src/java/com/zenesis/qx/remote/FileApi.java
+++ src/java/com/zenesis/qx/remote/FileApi.java
@@ -540,11 +540,12 @@ public class FileApi implements Proxied {
 
       File[] files = src.listFiles();
       boolean ok = true;
-        for (File file : files) {
-          moveTo(file, new File(dest, file.getName()));
-          if (file.exists())
-            ok = false;
+      for (File file : files) {
+        moveTo(file, new File(dest, file.getName()));
+        if (file.exists()) {
+          ok = false;
         }
+      }
       if (ok)
         src.delete();
       return;
