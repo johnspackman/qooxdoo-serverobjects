7cfa51778174d7e0415ddcfc4f21b16fe4fd8ab0
John Spackman <john.spackman@zenesis.com>
2025-05-19 19:34:27 +0100

    no message

--- src/java/com/zenesis/qx/remote/ProxyObjectMapper.java
+++ src/java/com/zenesis/qx/remote/ProxyObjectMapper.java
@@ -40,10 +40,7 @@ import org.apache.logging.log4j.Logger;
 import org.bson.Document;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
